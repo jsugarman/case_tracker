9fd31aa3bbe6481613c490959cd06ddc9decb919
jsugarman <joel.sugarman@digital.justice.gov.uk>
2014-07-02 21:38:37 +0100

    initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaseTracker::Application.config.secret_key_base = 'e78533ce7b82b597e3b64ebaea5de1e2ab72175561dc2fac23a04ebfa6844c4431c25498fb67446466993de30a0c90e4ee9729c57aaf4cdc831b8e00a73aa0f7'
