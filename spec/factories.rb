a2e194deb42e1d45d963bd805ebaea794aba062b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2014-07-24 17:09:50 +0100

    user model complete

---
+++ spec/factories.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+
+	factory :user do
+
+		name "John Smith"
+		email "john.smith@bigblackhole.com"
+		password "foobarish"
+		password_confirmation "foobarish"
+	end
+
+	
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe User do
 
-	before { @user = User.new(name: "Example User", email: "user@example.com",
-	 password:"foobarish", password_confirmation: "foobarish") }
-	subject { @user }
+	let(:user) { FactoryGirl.create(:user) } 
 
-  	describe "a valid user" do
+	# before { @user = User.new(name: "Example User", email: "user@example.com",
+	 # password:"foobarish", password_confirmation: "foobarish") }
+	subject { user }
+
+  	describe "a valid user responds to name, email, password_digest, password and password_confirmation" do
 		specify { expect(subject).to respond_to(:name) }
 		specify { expect(subject).to respond_to(:email) }
 		specify { expect(subject).to respond_to(:password_digest) }
@@ -18,70 +20,82 @@ describe User do
 	describe "an invalid user" do
 		
 		context "with no name" do
-			before { @user.name = '' }
+			before { user.name = '' }
 			specify { expect(subject).to_not be_valid }
 		end
 
 		context "with too short a name" do
-			before { @user.name = 'j' }
+			before { user.name = 'j' }
 			specify { expect(subject).to_not be_valid }
 		end
 
-		context "too long a name" do
-			before { @user.name = 'j'*129 }
+		context "with too long a name" do
+			before { user.name = 'j'*129 }
 			specify { expect(subject).to_not be_valid }
 		end
 
 		context "with no email" do
-			before { @user.email = '' }
-			specify { expect(subject).to_not be_valid }
-		end
-
-		context "with invalid format of email" do
-			before { @user.email = 'invalidemailaddress.com' }
+			before { user.email = '' }
 			specify { expect(subject).to_not be_valid }
 		end
 		
+		context "with invalid format of email" do
+			before { user.email = 'invalid.Mixed.com' }
+			specify { expect(subject).to_not be_valid }
+		end
+
 		pending "create more robust invalid mail format test using factories??!"
 		
 		context "with duplicate email" do
-			before do
-				user_with_same_email = @user.dup
-				user_with_same_email.name = 'different name'
-				user_with_same_email.save
-			end
-			specify { expect(subject).to_not be_valid }
+			# before do
+				# @user_with_same_email = user.dup
+				# user_with_same_email.name = 'different name'
+				# @user_with_same_email.save
+			# end
+			# it { should_not be_valid }
+			specify { expect(user.dup).to_not be_valid }
+			# specify { expect(subject).to_not be_valid }
 		end
 
 		context "with no password digest" do
-			before { @user.password_digest = "" }
+			before { user.password_digest = "" }
 			specify { expect(subject).to_not be_valid }
 		end
 
 		context "with mismatched password and confirmation" do
-			before { @user.password_confirmation = "not_foobar" }
+			before { user.password_confirmation = "not_foobar" }
 			specify { expect(subject).to_not be_valid }
 		end
 
 		context "with too short a password" do
-			before { @user.password = @user.password_confirmation = 'a'*7 }
+			before { user.password = user.password_confirmation = 'a'*7 }
 			specify { expect(subject).to_not be_valid }
 		end
 
 		pending "with password with one or more numbers or symbols"
+
+	end
+
+
+	describe "implicit data transformations" do
+
+		let(:mixed_case_email) { "foo.BAR@wherever.COM" }
 		
+		specify "mixed email case converted to lower case" do
 
-	# pending "should have email confirmation virtua field when creating???not creating though"
-
+			user.email = mixed_case_email
+			user.save
+			expect(user.reload.email).to eq mixed_case_email.downcase
+		end
 
 	end
 
 	describe "login password authentication" do
-		before { @user.save }
-		let(:valid_user) {User.find_by(email: @user.email)}
+		before { user.save }
+		let(:valid_user) {User.find_by(email: user.email)}
 
 		context "with valid password" do
-			specify { expect(valid_user.authenticate(@user.password)).to be_true }	
+			specify { expect(valid_user.authenticate(user.password)).to be_true }	
 		end
 
 		context "with invalid password" do
@@ -95,3 +109,5 @@ describe User do
 	pending "should be able to view other admins, if you are an admin"
 
 end
+
+
